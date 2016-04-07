4875811db16f61fa3b470aa7be56d7e4ce61d1eb
Pavel Liferenko <eleven.krsk@gmail.com>
2016-04-07 10:06:34 +0300

    trying to start gDrive API (error)

--- olx_robo.py
+++ olx_robo.py
@@ -4,27 +4,62 @@
 """ I was installing pip and PyDrive.
 Right now add from pypi.python.org/pypi/PyDrive
 1st step - adding OAuth..."""
-from pydrive.auth import GoogleAuth
-from pydrive.drive import GoogleDrive
+#!/usr/bin/python
 
-gauth = GoogleAuth()
-gauth.LocalWebserverAuth()
+import httplib2
+import pprint
 
-drive = GoogleDrive(gauth)
+from apiclient.discovery import build
+from apiclient.http import MediaFileUpload
+from oauth2client.client import OAuth2WebServerFlow
 
+
+# Copy your credentials from the APIs Console
+CLIENT_ID = '599191305513-idtd9kffcn4tncli9rpnag306auej7sf.apps.googleusercontent.com'
+CLIENT_SECRET = 'HeqlYjHowwf0DjOgw5yxicxr'
+
+# Check https://developers.google.com/drive/scopes for all available scopes
+OAUTH_SCOPE = 'https://www.googleapis.com/auth/drive'
+
+# Path to the file to upload
+FILENAME = 'document.txt'
+
+# Run through the OAuth flow and retrieve credentials
+flow = OAuth2WebServerFlow(CLIENT_ID, CLIENT_SECRET, OAUTH_SCOPE)
+authorize_url = flow.step1_get_authorize_url()
+print ('Go to the following link in your browser: ' + authorize_url)
+code = raw_input('Enter verification code: ').strip()
+credentials = flow.step2_exchange(code)
+
+# Create an httplib2.Http object and authorize it with our credentials
+http = httplib2.Http()
+http = credentials.authorize(http)
+
+drive_service = build('drive', 'v2', http=http)
+
+# Insert a file
+media_body = MediaFileUpload(FILENAME, mimetype='text/plain', resumable=True)
+body = {
+ 'title': 'My document',
+ 'description': 'A test document',
+ 'mimeType': 'text/plain'
+}
+
+file = drive_service.files().insert(body=body, media_body=media_body).execute()
+pprint.pprint(file)
 
 
 #RedGreenLight Test
-test = 1 #this var is RedGreenLight
+# test = 1 #this var is RedGreenLight
 
-def RGtest(test):
-	if test >= 0:
-		return "Green"
-	elif test == True:
-		return "Green"
-	else:
-		return Red
-print ("You have %s light" % (RGtest(test)))
+# def RGtest(test):
+# 	if test >= 0:
+# 		return "Green"
+# 	elif test == True:
+# 		return "Green"
+# 	else:
+# 		return Red
+# print ("You have %s light" % (RGtest(test)))
 #RedGreenLight Test
 
 				#2. Understand which raws I should listen and check; 
