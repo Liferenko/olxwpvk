509646614a883703b9b6379a602736d34277e19e
Pavel Liferenko <eleven.krsk@gmail.com>
2016-04-07 07:05:46 +0300

    There is bodies of 3 workers

---
+++ wp_robo.py
@@ -0,0 +1,29 @@
+#wp_robo
+
+				#1. Connect to gDrive spreadsheets 
+
+				#2. Understand which raws I should listen and check; 
+
+				#3. Detect new items 
+
+				#4. If I find new item, I should check am I adding this one in past? 
+
+				#5. If I see this item is brand new, I'm grabbing this data... 
+
+				#6. ... and I go to my canvac (my workspace) and ask it "Hey there. Can I come in?" (log in Wordpress Admin panel) 
+
+				#7. After successful login I'm creating new product... 
+
+				#8. ... and adding new title inside it... 
+
+				#9. ... and adding new descr... 
+
+				#10. ... and adding new thumbnail... 
+
+				#11. ... and adding new photos... 
+
+				#12. ... and adding new categories or just adding existing category 
+
+				#13. ... and I say:"Yeah, please publish it, love" 
+
+				#14. If all is okey, I start from to-do #1
