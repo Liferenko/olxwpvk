9c51d1bde86dfbd33bf4e6bfd28ab659abe65ec8
Paul Liferenko <eleven.krsk@gmail.com>
2016-04-16 14:34:16 +0000

    123

--- main.py
+++ main.py
@@ -2,8 +2,11 @@
 
 # Let me introduce our 3 respectful workers
 olx_robo = 'I\'m the one who add new items in OLX'
+
 wp_robo = 'I\'m adding new items in woocommerce market'
+
 vk_robo = 'I\'m the little, but powerful. And I\'m waiting for my little brother telegram_robo'
+
 
 print (olx_robo)
 print (wp_robo)
